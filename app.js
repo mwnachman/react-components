5a410646489d883198ec47fd36b7d78ed27a2408
Meredith Nachman <mwnachman@gmail.com>
2016-09-30 14:24:55 -0700

    blah

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// TODO
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// var Cabbage = () => (
+//   <li>Cabbage</li>
+// );
+
+// var Eggs = () => (
+//   <li>Eggs</li>
+// );
+
+// var GroceryListItem = (props) => (
+
+
+// )
+var GroceryListItem = (props) => ( 
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+    <li>{props.items[2]}</li>
+  </ul>
+);
+
+var GroceryList = () => (
+  <div>
+    <GroceryListItem items={['Eggs', 'Cabbage', 'Cheetos']}/>
+  </div>
+);
+    
+var App = () => (
+
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList /> 
+  {/* here we are creating an instance of the 'GroceryList' component -- also, this is the way to comment things*/}
+  </div>
+
+);
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
